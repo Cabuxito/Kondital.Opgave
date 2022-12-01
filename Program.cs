3c05d9fe7b41e5e73aeecda2c4ddf33559549aef
Brian Gastón di Gangi Pedersen <briandigangi@hotmail.com>
2022-12-01 15:32:47 +0100

    +Base for program

--- Program.cs
+++ Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿Console.WriteLine("Beregn kondital ud fra pulsværdier: ");
+double hvilepuls = IndtastAfVærdi("Indtast hvilepuls: ");
+double maxpuls = IndtastAfVærdi("Indtast maxpuls: ");
+double vægt = IndtastAfVærdi("Indtast din vægt: ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+static double IndtastAfVærdi(string textinput)
+{
+    double værdi;
+    while (true)
+    {
+        Console.Write(textinput);
+        try
+        {
+            return værdi = Convert.ToDouble(Console.ReadLine());
+        }
+        catch (Exception y)
+        {
+            Console.WriteLine("FAIL");
+        }
+    }
+}
